ae4c9fb9b9b93a9e4d28e4c8fd5fc612b123655e
ssafadi <samisaf@outlook.com>
2021-04-03 12:56:46 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import Navbar from "./Navbar";
 import CapsuleButton from "./CapsuleButton";
 
 export default function App() {
+  const [data, setData] = React.useState("");
+
+  React.useEffect(() => setData(fetchData()), [data]);
+
   return (
     <div className="">
       <Navbar>
         <div>Start Wars Info</div>
         <CapsuleButton>Planets</CapsuleButton>
+        <CapsuleButton color="green">People</CapsuleButton>
       </Navbar>
 
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
+}
+
+function fetchData() {
+  let result = {};
+  const url = "https://swapi.dev/api/people/1";
+  fetch(url).then(r => (result = r.json()));
+  console.log(result);
+  return result;
 }
--- src/CapsuleButton.js
+++ src/CapsuleButton.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 export default function CapsuleButton(props) {
-  const color = "border-blue-500 text-blue-500 hover:bg-blue-600";
-  const buttonStyle =
-    "p-1 m-1 border rounded-xl transition duration-500 ease select-none hover:text-white focus:outline-none focus:shadow-outline";
+  const color = props.color ? props.color : "blue";
+
+  const hover = `border-${color}-500 text-${color}-500 hover:bg-${color}-600 hover:text-white`;
+  const style =
+    "p-1 m-1 border rounded-xl transition duration-500 ease select-none focus:outline-none";
   return (
     <button
       type="button"
-      className={color + " " + buttonStyle}
+      className={`${hover} ${style}`}
       onClick={props.onClick}
     >
       {props.children}
