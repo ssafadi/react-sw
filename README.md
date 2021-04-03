3ec28ca816516062a2e8c124e2ddb2116fb7cc0c
ssafadi <samisaf@outlook.com>
2021-04-03 10:36:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sw)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="bg-gray-100">
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
